d513e472171aedaf5a1121981854d64afe014d7e
Genji Fujimori <30670749+ahandsel@users.noreply.github.com>
2021-02-15 17:12:31 +0900

    Create imgur.js

---
+++ imgur.js
@@ -0,0 +1,8 @@
+// The photos of the 46 US presidents are also publicly shared on imgur
+
+  // Imgur's Image Hash
+  const imgCode = ['', 'gk1cP2u', '9FR7eva', 'aumfl2F', 'kx8BdCT', 'z6Yd9Lm', '4HLWKcB', 'mmTyKJ1', 'PCb7ly6', 'UgN4EA7', 'rVoiW4X', 'i3euA9w', 'kt92UVi', 'oo5aw5x', 'BZI7FTa', 'XLkIWAs', 'qB4GVSv', '5kAZ3NJ', 'drQysn9', 'UqvMVTH', 'Wte60Q6', '3DAK1C1', 'h6b7Vw5', 'ctHNmhA', '18zBca0', 'hb94eO2', '12hJdg0', 'Bl8oRVY', 'HQ1xV6H', 'pj4oAjR', 'QuzzGmm', 'zpE50Pa', '4zSuesA', 'mevGXAh', '5zq1fDg', 'EcUoEs2', 'S5BJfEO', 'bMW0Ltr', 'hutxiiz', 'hRBgyF1', 'gHP21eA', '3QtuHuW', 'XSwwVLh', 'wnbeSN1', 'Uw18RFA', '4APgxYd', 'S5wCZcd'];
+
+function getImage(PresidentNum) {
+  return URL = `https://i.imgur.com/${imgCode[PresidentNum]}.png`;
+}
